03c93a22f7508f35c60826f2164835e0e04d756b
Huy Pham <huypq@smartosc.com>
2016-02-14 16:05:40 +0700

    i love you, but...

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,3 +1,28 @@
+;(function(window) {
+  function init() {
+    var itemElems = document.getElementsByClassName('item-on-cart');
+    for (var _i = 0; _i < itemElems.length; _i++) {
+      var _extend = itemElems[_i].getElementsByClassName('extend')[0];
+      _extend.addEventListener('click', function(event) {
+        event.stopPropagation();
+      }, false);
+    }
+  }
+
+  function itemOnCartClickHandle(event) {
+    event.currentTarget.classList.toggle('active');
+  }
+  function itemExtendOnCartClickHandle(event) {
+    event.stopPropagation();
+  }
+
+  init();
+  //add to global
+  window.itemOnCartClickHandle = itemOnCartClickHandle;
+  window.itemExtendOnCartClickHandle = itemExtendOnCartClickHandle;
+
+})(window);
+
 ;(function(window, undefined) {
   function init() {
     window.showToSaleElem = document.getElementById('show-to-sale');
